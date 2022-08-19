d0cbfe8394b723c7c23cc6755a31b04602f13c74
NaufalNAK <88518542+NaufalNAK@users.noreply.github.com>
2022-08-19 10:20:14 +0700

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,48 @@
+const getData = () => {
+  return [
+    {
+      id: 1,
+      name: "Arief Firmansyah",
+      tag: "812-8300-7626",
+      imageUrl: "/images/arief.jpg",
+    },
+    {
+      id: 2,
+      name: "Alvito Reyhan",
+      tag: "821-1221-1757",
+      imageUrl: "/images/default.jpg",
+    },
+    {
+      id: 3,
+      name: "Dimas Aulia Rahman",
+      tag: "858-8054-8723",
+      imageUrl: "/images/dimasaulia.jpeg",
+    },
+    {
+      id: 4,
+      name: "Faiq Bangkit Wicaksono",
+      tag: "812-8129-0895",
+      imageUrl: "/images/faiqbangkit.jpeg",
+    },
+    {
+      id: 5,
+      name: "Gilang Rizky Putra",
+      tag: "858-1039-3460",
+      imageUrl: "/images/gilangrizky.jpeg",
+    },
+    {
+      id: 6,
+      name: "Naufal Andresya Kholish",
+      tag: "895-4025-73900",
+      imageUrl: "/images/nafl.jpg",
+    },
+    {
+      id: 7,
+      name: "Singgih Rahbanu",
+      tag: "812-1239-9730",
+      imageUrl: "/images/default.jpg",
+    },
+  ];
+};
+
+export { getData };
